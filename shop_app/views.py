6c00f4bb2f372904319753696936d4905ed3e362
Binay Shrestha <binayshrestha798@gmail.com>
2020-03-27 16:10:16 +0545

    Sign Up created

--- shop_app/views.py
+++ shop_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Items
+from django.contrib.auth.models import User,auth
 # Create your views here.
 def index(request):
     items = Items.objects.all()
@@ -7,7 +8,20 @@ def index(request):
     return render(request,'index.html',context)
 
 def signin(request):
-    return render(request,'signin.html')
+    if request.method == 'POST':
+        firstname = request.POST['fname']
+        lastname = request.POST['lname']
+        username = request.POST['uname']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        user = User.objects.create_user(first_name=firstname,last_name=lastname,username=username,email=email,password=password1)
+        user.save()
+        return redirect("/")
+    else:
+
+        return render(request,'signin.html')
 
 def profile(request):
     return render(request,'profile.html')
